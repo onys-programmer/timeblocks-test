7f5bcbdba067d6619ac9e094d763db7badf296f0
onys <thkim2017@gmail.com>
2022-08-25 13:53:24 +0900

    test: 💍 refactoring: 현재 연도와 month now.js에서 import해서 test하게

--- src/Calendar.test.jsx
+++ src/Calendar.test.jsx
@@ -1,15 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import Calendar from './Calendar';
+import { year, month } from './now';
 
 describe('Calendar는', () => {
   it('현재 연도를 렌더합니다.', () => {
     render(<Calendar />);
-    const linkElement = screen.getByText(/2022/i);
-    expect(linkElement).toBeInTheDocument();
+    const yearElenent = screen.getByText(year);
+    expect(yearElenent).toBeInTheDocument();
   });
+
   it('현재 Month를 렌더합니다.', () => {
     render(<Calendar />);
-    const linkElement = screen.getByText(/8/i);
-    expect(linkElement).toBeInTheDocument();
+    const monthElement = screen.getByText(month);
+    expect(monthElement).toBeInTheDocument();
   });
 });
