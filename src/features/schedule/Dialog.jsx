1a5f7b82eba710ee4a04ae03e25ba7b09df6804f
onys <thkim2017@gmail.com>
2022-08-26 22:02:00 +0900

    fix: 🐛 엔터 쳐서 submit 안되는 버그 수정
    button type 설정

--- src/features/schedule/Dialog.jsx
+++ src/features/schedule/Dialog.jsx
@@ -6,7 +6,7 @@ function Dialog({ dateString, onChange, onSubmit, onClick }) {
       <form
         onSubmit={onSubmit}
       >
-        <button onClick={onClick}>X</button>
+        <button type="button" onClick={onClick}>X</button>
         <h2>{dateString}</h2>
         <br></br>
         <input
@@ -14,7 +14,7 @@ function Dialog({ dateString, onChange, onSubmit, onClick }) {
           onChange={onChange}
           placeholder="일정을 입력해주세요."
         />
-        <button>확인</button>
+        <button type="submit">확인</button>
       </form>
     </div>
   );
